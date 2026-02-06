52798ef2476f0608ec3015c3bc0c83a8c446a2ba
Capumaf <cesarpumayallaf@gmail.com>
2026-02-06 19:21:53 +0100

    Update footer yea

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="w-full py-6 text-center">
       <p className="text-sm tracking-wide text-neutral-500">
-        IngridPumayalla.com © All Rights Reserved. · Powered by NinanStudio
+        IngridPumayalla.com - All Rights Reserved © Powered by NinanStudio
       </p>
     </footer>
   );
