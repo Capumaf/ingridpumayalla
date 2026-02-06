6d5b491ca3db8fa1516794330484be42bbebb04a
Capumaf <cesarpumayallaf@gmail.com>
2026-02-06 22:13:33 +0100

    Limit image height to avoid scroll

--- app/(chrome)/home/page.js
+++ app/(chrome)/home/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <div className="w-full max-w-[550px] px-6">
-      <div className="relative w-full aspect-[3/4]">
+      <div className="relative w-full aspect-[3/4] max-h-[calc(100vh-140px)]">
         <Image
           src="/curiwarmiLunar2.png"
           alt=""
