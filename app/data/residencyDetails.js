cb10f51c444432fdd33c174b9b0da5de30399ec7
Capumaf <cesarpumayallaf@gmail.com>
2026-05-16 11:16:50 -0500

    Fix residency image filenames

--- app/data/residencyDetails.js
+++ app/data/residencyDetails.js
@@ -47,17 +47,17 @@ export const residencyDetails = {
   },
 
     "pa-utrygg-grun": {
-    title: "Pa Utrygg grun",
-    introduction: "Residency project.",
+  title: "Pa Utrygg grun",
+  introduction: "Residency project.",
 
-    imageData: [
-      { id: "1", src: "/pautrygggrun1.webp", width: 1200, height: 800 },
-      { id: "2", src: "/pautrygggrun2.webp", width: 1200, height: 800 },
-      { id: "3", src: "/pautrygggrun3.webp", width: 1200, height: 800 },
-      { id: "4", src: "/pautrygggrun4.webp", width: 1200, height: 800 },
-      { id: "4", src: "/pautrygggrun4.webp", width: 1200, height: 800 },
-    ]
-  } ,
+  imageData: [
+    { id: "1", src: "/pautrygggrun1.webp", width: 1200, height: 800 },
+    { id: "2", src: "/pautrygggrun2.webp", width: 1200, height: 800 },
+    { id: "3", src: "/pautrygggrun3.webp", width: 1200, height: 800 },
+    { id: "4", src: "/pautrygggrun4.webp", width: 1200, height: 800 },
+    { id: "5", src: "/pautrygggrun5.webp", width: 1200, height: 800 },
+  ]
+},
 
   "stokkoy-art-carpeta-": {
   title: "Stokkoy Art Carpeta sin título",
