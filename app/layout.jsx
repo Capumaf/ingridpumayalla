fa18d9325442a3fa908ade4569bcd75b1c2163d2
Capumaf <cesarpumayallaf@gmail.com>
2026-06-16 16:08:16 -0500

    Add Vercel Speed Insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,12 @@
-// app/layout.jsx
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <body className="h-full m-0" suppressHydrationWarning>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
